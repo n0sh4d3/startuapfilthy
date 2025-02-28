2d8ef4bad1a38794af51bf6e55038d6ca0f5f54e
n0_sh4d3 <big_fat@cock.edu>
2025-02-28 22:13:52 +0100

    interaktywne populowanie chujstwa

--- app/app.py
+++ app/app.py
@@ -3,6 +3,44 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+NAME = "name"
+DESCRIPTION = "description"
+PRICE = "price"
+
+shop_items = [
+    {
+    NAME: "Item1",
+    DESCRIPTION: "Opis1",
+    PRICE: 10.99,
+    },
+    {
+    NAME: "Item2",
+    DESCRIPTION: "Opis2",
+    PRICE: 20.99,
+    },
+]
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html', shop_items=shop_items)
+
+@app.route('/cart')
+def cart():
+    cart_items = shop_items
+    total = 0
+    for item_price in cart_items:
+        total += item_price[PRICE]
+
+    total = round(total, 2)
+    return render_template('cart.html', cart_items=cart_items, total=total)
