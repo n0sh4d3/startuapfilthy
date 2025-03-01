a2c6b92df2afcb6760b6ffa15a27adbe2456692a
n0_sh4d3 <big_fat@cock.edu>
2025-03-01 15:48:43 +0100

    zjebalem prod

--- app/app.py
+++ app/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, make_response, abort
-import app.items_db as items_db
 
 app = Flask(__name__)
 
@@ -8,6 +7,43 @@ DESCRIPTION = "description"
 PRICE = "price"
 SIZE = "size"
 
+shop_items = [
+    {
+        NAME: "spust pycha",
+        DESCRIPTION: "nie wiem co sie dzieje ale kurwa buja nie",
+        PRICE: 21.37,
+    },
+    {
+        NAME: "brudna woda",
+        DESCRIPTION: "nazwa muwi sama za sb",
+        PRICE: 20.99,
+    },
+    {
+        NAME: "order cwela",
+        DESCRIPTION: "ciezko zapracowalem na ten tytul",
+        PRICE: 90.99,
+    },
+    {
+        NAME: "zestaw goonera",
+        DESCRIPTION: "zeby hojnie zyc trzeba hojnie wydac",
+        PRICE: 0.00, # robiac to licze sie z faktem ze kosz sie rozjebie ale to smiesznie
+    },
+    {
+        NAME: "cyberszlug",
+        DESCRIPTION: "smakowe poietrze goes brrrrr",
+        PRICE: 1.99,
+    },
+    {
+        NAME: "chinska republika ludowa",
+        DESCRIPTION: "Zǎoshang hǎo zhōngguó xiànzài wǒ yǒu BING CHILLING 🥶🍦 wǒ hěn xǐhuān BING CHILLING 🥶🍦",
+        PRICE: 4.50,
+    },
+    {
+        NAME: "samohud",
+        DESCRIPTION: "ten ruw wyglada bardzo hottt",
+        PRICE: 1999.99,
+    },
+]
 
 @app.route('/')
 def home():
@@ -24,14 +60,14 @@ def contact():
 
 @app.route('/shop')
 def shop():
-    shop_items = items_db.get_shop_items()
+    shop_items = shop_items
     return render_template('shop.html', shop_items=shop_items)
 
 @app.route('/item/<item_name>')
 def item(item_name):
     
     # modl sie ze nastepny item istnieje
-    item = next((item for item in get_shop_items if item[NAME].lower() == item_name.lower()), None)
+    item = next((item for item in shop_items if item[NAME].lower() == item_name.lower()), None)
 
     if item is None:
         print(f"Item not found: {item_name}")  # pytanie kurwa jak skoro jest. patrze sie na to kurwa
@@ -43,7 +79,7 @@ def item(item_name):
 
 @app.route('/cart')
 def cart():
-    cart_items = get_shop_items
+    cart_items = shop_items
     total = 0
     for item in cart_items:
         total += item[PRICE]
